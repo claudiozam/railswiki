32674f4b190be11e1738d58698709f22f8dadc7a
claudio <claudio@honou.com.ar>
2015-03-30 12:34:45 -0300

    device - twitter bootstrap rails - user and category model

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 source 'https://rubygems.org'
 
+ruby '2.1.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# Use pos as the database for Active Record
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -23,6 +24,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'devise'
+gem "twitter-bootstrap-rails"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -42,5 +45,8 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  # Use FFaker for random seed generation: https://github.com/EmmanuelOga/ffaker
+  gem 'ffaker'
 end
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,64 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_categories, only: [:edit, :update, :new, :create]
+
+  # GET /articles
   def index
-  	@articles = Article.all
+    @articles = Article.all
   end
+
+  # GET /articles/1
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+    	render :new
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated.'
+    else
+    	render :edit
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article.destroy
+    redirect_to articles_url, notice: 'Article was successfully destroyed.' 
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def set_categories
+    	@categories = Category.order("name")
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def article_params
+      params.require(:article).permit(:title, :description, :category_id)
+    end
 end
